74cbce0ea13a8ef34b33afde8228edcc19d32dae
Sylvain Cyr <scyr@solutionextreme.com>
2021-04-30 21:20:51 -0400

    moved to version 2021-04

--- src/AbstractApi.php
+++ src/AbstractApi.php
@@ -8,7 +8,7 @@ use Shopify\Service\AbstractService;
 
 abstract class AbstractApi implements ApiInterface
 {
-    const DEFAULT_API_VERSION = '2019-04';
+    const DEFAULT_API_VERSION = '2021-04';
 
     /**
      * Domain of the Shopify store
